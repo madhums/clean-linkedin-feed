9ae5620a5d762d24d830e4e4b2995e6da9a31f27
madhums <me@madhums.me>
2023-08-30 10:24:29 +0200

    update css

--- background.js
+++ background.js
@@ -1,17 +1,16 @@
 chrome.tabs.onActivated.addListener(cleanFeed);
-chrome.webNavigation.onCompleted.addListener(cleanFeed)
+chrome.webNavigation.onCompleted.addListener(cleanFeed, {
+  url: [{ hostContains: "linkedin.com" }, { hostPrefix: "www" }],
+})
 
 function cleanFeed(active) {
   chrome.tabs.get(active.tabId, function (tab) {
-    if (tab.url && (tab.url.includes('://www.linkedin.com/') ||
-      tab.url.includes('://linkedin.com/'))) {
-      chrome.tabs.insertCSS(active.tabId, { code: cleanupCSS() }, () => {
-        console.log('Cleaned up linkedin feed CSS')
-      });
-      chrome.tabs.executeScript(active.tabId, { file: 'clf.js' }, () => {
-        console.log('Cleaned up linkedin feed JS')
-      });
-    }
+    chrome.tabs.insertCSS(active.tabId, { code: cleanupCSS() }, () => {
+      console.log('Cleaned up linkedin feed CSS')
+    });
+    chrome.tabs.executeScript(active.tabId, { file: 'clf.js' }, () => {
+      console.log('Cleaned up linkedin feed JS')
+    });
   });
 }
 
@@ -19,9 +18,12 @@ function cleanupCSS() {
   return `
     /* image in link preview */
     .feed-shared-article__link-container, .feed-shared-article__image-link,
+    .update-components-article__link-container,
 
     /* image */
     .feed-shared-image, .feed-shared-image__container,
+    .update-components-image__container,
+    .feed-shared-celebration-image,
 
     /* video */
     .feed-shared-linkedin-video, .media-player,
@@ -30,7 +32,8 @@ function cleanupCSS() {
     .feed-shared-external-video__container,
 
     /* documents and presentations */
-    .feed-shared-linkedin-document__container, .feed-shared-document, iframe {
+
+    .feed-shared-document__container, .feed-shared-document, iframe {
       display: none !important;
       width: 0 !important;
       height: 0 !important;
